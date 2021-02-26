7c839ff93eb6e4b65bde94e9d3f8332b2b565707
Ladislav <szolik.ladislav@gmail.com>
2021-02-26 17:02:00 +0100

    fix grid spacing

--- components/BookThumbnail.js
+++ components/BookThumbnail.js
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 const BookThumbnail = ({ title, author, url }) => {
   return (
-    <div >
+    <>
       <div className="w-28">
-        <Image
-          alt={title}          
-          width={230}
-          height={398}
-          src={`/books/${url}`}
-        />
+        <Image alt={title} width={230} height={398} src={`/books/${url}`} />
       </div>
-      <p className="font-medium text-base text-gray-700"> {title} <span className="block pt-2 font-normal uppercase text-sm text-gray-400">{author}</span></p>
-      
-    </div>
+      <p className="font-medium text-base text-gray-700">
+        {" "}
+        {title}{" "}
+        <span className="block pt-2 font-normal uppercase text-sm text-gray-400">
+          {author}
+        </span>
+      </p>
+    </>
   );
 };
 
